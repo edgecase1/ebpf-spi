02178e5a5ca4ec5c9744654d5879a66e01002c52
Reinhard <rndlab@gmx.at>
2024-05-07 23:04:18 +0200

    it can trace

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@
 all: spyspi.bpf.o link
 	echo "all"
 
-spyspi.bpf.o: spyspi.bpf.c vmlinux
-	clang -g -O2 -target bpf -D__TARGET_ARCH_x86_64 -I . -c spyspi.bpf.c -o spyspi.bpf.o
-#	clang -O2 -g -target bpf -I. -D __TARGET_ARCH_arm64 -D__BPF_TRACING__ -c spyspi.bpf.c -o spyspi.bpf.o
+spyspi.bpf.o: 
+	#clang -g -O2 -target bpf -D__TARGET_ARCH_x86_64 -I . -c spyspi.bpf.c -o spyspi.bpf.o
+	lang -O2 -g -target bpf -I. -D __TARGET_ARCH_arm64 -D__BPF_TRACING__ -c spyspi.bpf.c -o spyspi.bpf.o
 
 load: 
 	bpftool prog load spyspi.bpf.o /sys/fs/bpf/spyspi autoattach
--- spyspi.bpf.c
+++ spyspi.bpf.c
@@ -1,19 +1,65 @@
-#include "vmlinux.h"
-//#include <linux/bpf.h>
+////#include "vmlinux.h"
+
+
+//#include <linux/types.h>
+//#include <linux/compiler_types.h>
+//
+
+
+#include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+#include <stdbool.h>
+
 //#include <linux/spi/spi.h>
+//#include <linux/device.h>
+
+struct device {
+	void *kobj;
+	void *parent;
+	void *p;
+	char *init_name;
+};
+
+struct spi_device {    // linux/spi/spi.h
+  struct device dev;   // linux/device.h
+  void *master;	       // struct spi_master	
+  unsigned max_speed_hz;		       // 
+  unsigned int chip_select; //u8
+};
+
+struct spi_set_cs_args
+{
+    unsigned short common_type;
+    unsigned char common_flags;
+    unsigned char common_preempt_count;
+    int common_pid;
+    
+    int bus_num;
+    int chip_select;
+    unsigned long mode;
+    bool enable;
+};
+
+
+struct spi_transfer_start_args
+{
+    unsigned short common_type;
+    unsigned char common_flags;
+    unsigned char common_preempt_count;
+    int common_pid;
+    
+    int bus_num;
+    int chip_select;
+    struct spi_transfer * xfer;
+    int len;
+    unsigned int rx_buf[4];
+    unsigned int tx_buf[4];
+};
 
 char LICENSE[] SEC("license") = "GPL";
 
-struct spi_controller_args {
-   uint16_t common_type;
-	uint8_t common_flags;
-	uint8_t common_preempt_count;
-	int32_t common_pid;
-	int bus_num;
-};
 
-SEC("tracepoint/spi/spi_controller_idle")
+SEC("raw_tracepoint/spi/spi_controller_idle")
 int idle22(struct spi_controller_args *ctx)
 {
     bpf_printk("SPI idle!\n");
@@ -27,9 +73,36 @@ int busy22(struct bpf_raw_tracepoint_args *ctx)
     return 0;
 }
 
-SEC("raw_tracepoint/spi/spi_message_start")
-int msgstart22(struct spi_message *msg, struct spi_transfer *xfer)
+SEC("tracepoint/spi/spi_transfer_start")
+int transferstart22(struct spi_transfer_start_args *ctx) //struct spi_message *msg, struct spi_transfer *xfer)
 {
-    bpf_printk("SPI msg start!\n");
+    char tx[4];
+    bpf_printk("SPI msg start! bus=%d cs=%d xfer=%x", ctx->bus_num, ctx->chip_select, ctx->xfer);
+    //bpf_probe_read(&tx, 4, ctx->tx_buf);
+    //bpf_printk("payload='%x'", tx);
+
+    return 0;
+}
+
+SEC("raw_tracepoint/spi/spi_message_submit")
+int submit22(struct spi_message *msg, struct spi_transfer *xfer)
+{
+    bpf_printk("SPI msg submit!\n");
+    return 0;
+}
+
+//SEC("raw_tracepoint/spi/spi_set_cs")
+SEC("tracepoint/spi/spi_set_cs")
+//int spisetcs22(struct spi_device *spi, bool enable)
+//int spisetcs22(struct bpf_raw_tracepoint_args *ctx) 
+int spisetcs22(struct spi_set_cs_args *ctx) 
+{
+    //bpf_probe_read(&myspi, sizeof(struct spi_device), (const void *)spi);
+    //bpf_probe_read(&myspi, sizeof(struct spi_device), (const void *)spi);
+    //bpf_probe_read(&max_speed_hz, sizeof(unsigned int), (const void *)spi->max_speed_hz);
+    //bpf_printk("max hz %u", myspi.max_speed_hz); //((struct device)spi->dev).init_name); //.init_name);
+    //bpf_printk("chip select %x", spi->chip_select); //((struct device)spi->dev).init_name); //.init_name);
+    //bpf_printk("device %s", dev->init_name); //((struct device)spi->dev).init_name); //.init_name);
+    bpf_printk("spi_set_cs bus=%d mode=%lu enable=%d\n", ctx->bus_num, ctx->mode, ctx->enable); //((struct device)spi->dev).init_name); //.init_name);
     return 0;
 }
